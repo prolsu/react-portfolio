520fc34f206a1a2f7877ec8d77ff19a196be504f
Pedro Alburqueque <prolsu88@gmail.com>
2021-03-28 22:07:17 -0400

    updates to package.json and build folder

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,9 @@
 import React from "react";
 
-import Wrapper from "../components/Wrapper";
 import ContactC from "../components/ContactC";
 
 function Contact() {
-    return (
-        <Wrapper>
-            <ContactC />
-        </Wrapper>
-    );
+    return <ContactC />
 };
 
 export default Contact;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,16 +1,9 @@
 import React from "react";
 
-import Wrapper from "../components/Wrapper";
 import PortfolioC from "../components/PortfolioC"; 
 
 function Portfolio() {
-    return (
-        <div>
-            <Wrapper>
-                <PortfolioC />
-            </Wrapper>
-        </div>
-    );
+    return <PortfolioC />
 };
 
 export default Portfolio;
